355580855322332c4120195733dc859e89e91f85
Marc Breitung <mbreitung@drole.de>
2017-11-01 13:16:04 +0100

    added url to package.json

--- test/Unit/Search/Strategy/DepthFirstSearchTest.js
+++ test/Unit/Search/Strategy/DepthFirstSearchTest.js
@@ -21,11 +21,15 @@ describe('DepthFirstSearch', function () {
         let graphNodeD = new GraphNode('D', new Point(3, 1), new Point(30, 10));
         let graphNodeE = new GraphNode('E', new Point(3, 2), new Point(30, 20));
 
+        let graphNodeX = new GraphNode('X', new Point(5, 5), new Point(50, 50));
+        let graphNodeY = new GraphNode('Y', new Point(6, 6), new Point(60, 60));
+
         graphNodeA.addConnections([graphNodeB, graphNodeD, graphNodeE]);
         graphNodeB.addConnections([graphNodeA, graphNodeC]);
         graphNodeC.addConnections([graphNodeB, graphNodeD]);
         graphNodeD.addConnections([graphNodeA, graphNodeE, graphNodeC]);
         graphNodeE.addConnections([graphNodeA, graphNodeD]);
+        graphNodeX.addConnections([graphNodeY]);
 
         graph = new Graph();
         graph.addNode(graphNodeA);
@@ -33,6 +37,8 @@ describe('DepthFirstSearch', function () {
         graph.addNode(graphNodeC);
         graph.addNode(graphNodeD);
         graph.addNode(graphNodeE);
+        graph.addNode(graphNodeX);
+        graph.addNode(graphNodeY);
     });
 
     describe('#recursiveSearch(node, problem)', function () {
@@ -41,7 +47,6 @@ describe('DepthFirstSearch', function () {
             let goal = new State('B');
             let problem = new Problem(graph, initialState, goal);
             let nodeA = new Node(initialState);
-            let nodeB = Node.make(problem, nodeA, new Action('B'));
 
             let depthFirstSearch = new DepthFirstSearch();
             depthFirstSearch.recursiveSearch(nodeA, problem);
@@ -50,13 +55,16 @@ describe('DepthFirstSearch', function () {
         });
         it('should return the node if given node is the goal', function () {
             let initialState = new State('A');
-            let goal = new State('A');
+            let goal = new State('C');
             let problem = new Problem(graph, initialState, goal);
-            let nodeA = new Node(initialState);
 
             let depthFirstSearch = new DepthFirstSearch();
 
-            assert.sameDeepMembers(depthFirstSearch.search(problem).solution(), [nodeA]);
+            let nodeA = new Node(initialState);
+            let nodeB = Node.make(problem, nodeA, new Action('B'));
+            let nodeC = Node.make(problem, nodeB, new Action('C'));
+
+            assert.sameDeepMembers(depthFirstSearch.search(problem).solution(), [nodeA, nodeB, nodeC]);
         });
     });
 
