6255621ed0e517872c1377c8dd168a94caae8e2f
Marc Breitung <info@marcbreitung.de>
2018-03-05 15:21:58 +0100

    modified StateTest

--- test/Unit/Search/StateTest.js
+++ test/Unit/Search/StateTest.js
@@ -1,13 +1,17 @@
 let assert = require('chai').assert;
 
+import {GraphNode} from "../../../lib/Graph/GraphNode";
+import {Point} from "../../../lib/Graph/Point";
 import {State} from './../../../lib/Search/State';
 
 suite('State', function () {
 
     suite('#constructor(id)', function () {
-        test('should set the property id', function () {
-            let state = new State('State ID');
+        test('should set the property id and node', function () {
+            let graphNode = new GraphNode('A', new Point(2, 2), new Point(20, 20));
+            let state = new State('State ID', graphNode);
             assert.propertyVal(state, 'id', 'State ID');
+            assert.deepPropertyVal(state, 'node', graphNode);
         });
     });
 
